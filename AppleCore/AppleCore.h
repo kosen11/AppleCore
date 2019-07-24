40b0e8ad8510c281787daa7bba907e048dc5c078
Raymond Pietz <kosen11@gmail.com>
2019-07-24 10:44:19 -0700

    Initial Commit

---
+++ AppleCore/AppleCore.h
@@ -0,0 +1,19 @@
+//
+//  AppleCore.h
+//  AppleCore
+//
+//  Created by Ray Pietz on 7/24/19.
+//  Copyright © 2019 talech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppleCore.
+FOUNDATION_EXPORT double AppleCoreVersionNumber;
+
+//! Project version string for AppleCore.
+FOUNDATION_EXPORT const unsigned char AppleCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleCore/PublicHeader.h>
+
+
