b662b2f334741263589352f968228c7306e14e3a
Raymond Pietz <kosen11@gmail.com>
2019-09-23 15:23:13 -0700

    reorganize Apple Core folders

---
+++ AppleCore/UIKit/CollectionView/ExpandableCell.swift
@@ -0,0 +1,77 @@
+//
+//  ExpandableCell.swift
+//  AppleCore
+//
+//  Created by Ray Pietz on 9/23/19.
+//  Copyright © 2019 talech Inc. All rights reserved.
+//
+
+import UIKit
+
+open class ExpandableCell: UICollectionViewCell {
+    private var initialFrame: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+    private var initialCornerRadius: CGFloat = 0.0
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowRadius = contentView.layer.cornerRadius
+    }
+    
+    public func expand(in collectionView: UICollectionView) {
+        initialFrame = self.frame
+        initialCornerRadius = self.contentView.layer.cornerRadius
+        
+        self.contentView.layer.cornerRadius = 0
+        self.frame = CGRect(x: 0, y: collectionView.contentOffset.y, width: collectionView.frame.width, height: collectionView.frame.height)
+        
+        layoutIfNeeded()
+    }
+    
+    public func collapse() {
+        self.contentView.layer.cornerRadius = initialCornerRadius
+        self.frame = initialFrame
+        
+        layoutIfNeeded()
+    }
+    
+    public func hide(in collectionView: UICollectionView, frameOfSelectedCell: CGRect) {
+        initialFrame = self.frame
+        
+        let currentY = self.frame.origin.y
+        let newY: CGFloat
+        
+        if currentY < frameOfSelectedCell.origin.y {
+            let offset = frameOfSelectedCell.origin.y - currentY
+            newY = collectionView.contentOffset.y - offset
+        } else if currentY > frameOfSelectedCell.origin.y {
+            let offset = currentY - frameOfSelectedCell.maxY
+            newY = collectionView.contentOffset.y + collectionView.frame.height + offset
+        } else {
+            let offset = frameOfSelectedCell.maxX
+            newY = collectionView.contentOffset.y + collectionView.frame.height + offset
+        }
+        
+        self.frame.origin.y = newY
+        
+        layoutIfNeeded()
+    }
+    
+    public func show() {
+        self.frame = initialFrame
+        
+        layoutIfNeeded()
+    }
+}
