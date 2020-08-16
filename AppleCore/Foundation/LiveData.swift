660060a6ea84abbae76f3017f4958f2fb1cb53c1
Raymond Pietz <kosen11@gmail.com>
2020-08-16 10:20:08 -0700

    LiveData

---
+++ AppleCore/Foundation/LiveData.swift
@@ -0,0 +1,41 @@
+//
+//  LiveData.swift
+//  AppleCore
+//
+//  Created by Ray Pietz on 8/16/20.
+//  Copyright © 2020 talech Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol MutableData {
+    var notificationName: String { get }
+}
+
+class LiveData<T: MutableData> {
+    
+    init(_ startValue: T) {
+        _value = startValue
+        notify()
+    }
+    
+    private var _value: T
+    var value: T {
+        get { return _value }
+        set {
+            _value = newValue
+            notify()
+        }
+    }
+    
+    public func addObserver(observer: Any, selector: Selector, object: Any?) {
+        NotificationCenter.default.addObserver(observer,
+                                               selector: selector,
+                                               name: NSNotification.Name(rawValue: _value.notificationName),
+                                               object: object)
+    }
+    
+    private func notify() {
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: _value.notificationName), object: self, userInfo: ["value": value])
+    }
+}
---
+++ AppleCore/Foundation/VoidFunction.swift
@@ -0,0 +1,11 @@
+//
+//  VoidFunction.swift
+//  AppleCore
+//
+//  Created by Ray Pietz on 8/16/20.
+//  Copyright © 2020 talech Inc. All rights reserved.
+//
+
+import Foundation
+
+typealias VoidFunction = (() -> Void)
